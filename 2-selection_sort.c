25ef604dc3e4f9d4c57a90861ce5cb703caffede
Christian Koranteng <korantengchristian@gmail.com>
2022-10-08 18:46:05 +0000

    Update files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,41 +2,41 @@
 #include "sort.h"
 
 /**
-* swap - function to swap two integers
-*
-* @a: first variable
-* @b: second variable
-*/
+ * swap - function to swap two integers
+ *
+ * @a: first variable
+ * @b: second variable
+ */
 void swap(int *a, int *b)
 {
-int holding_variable = *a;
-*a = *b;
-*b = holding_variable;
+  int holding_variable = *a;
+  *a = *b;
+  *b = holding_variable;
 }
 
 /**
-* selection_sort - function to sort using the selection
-* sort algorithm
-*
-* @array: the array passsed to the function
-* @size: size of the array
-*
-* Return: nothing
-*/
+ * selection_sort - function to sort using the selection
+ * sort algorithm
+ *
+ * @array: the array passsed to the function
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
 void selection_sort(int *array, size_t size)
 {
-size_t j, i;
+  size_t j, i;
 
-for (j = 0; j < size - 1; j++)
-{
-int minimum = j;
-for (i = j + 1; i < size; i++)
-{
-if (array[i] < array[minimum])
-minimum = i;
-}
+  for (j = 0; j < size - 1; j++)
+  {
+    int minimum = j;
+    for (i = j + 1; i < size; i++)
+    {
+      if (array[i] < array[minimum])
+        minimum = i;
+    }
 
-swap(&array[minimum], &array[j]);
-print_array(array, size);
-}
+    swap(&array[minimum], &array[j]);
+    print_array(array, size);
+  }
 }
