81bff9655da678698ed3f2c859cd1bf4d495e48d
Christian Koranteng <korantengchristian@gmail.com>
2022-10-08 15:05:34 +0000

    Update files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,30 +3,32 @@
 #include "sort.h"
 
 /**
- * bubble_sort - function to sort an integer array in
- * ascending order
- *
- * @array: the input array
- * @size: size of the array
- *
- * Return: always nothing
- */
+* bubble_sort - function to sort an integer array in
+* ascending order
+*
+* @array: the input array
+* @size: size of the array
+*
+* Return: always nothing
+*/
 
 void bubble_sort(int *array, size_t size)
 {
-  int swap_variable;
-  size_t iterator, i;
+int swap_variable;
+size_t iterator, i;
 
-  for (iterator = 0; iterator < size - 1; ++iterator)
-  {
-    for (i = 0; i < size - (iterator - 1); ++i) /*Loop through and compare elements*/
-    {
-      if (array[i] > array[i + 1]) /*change > to < for descending order*/
-      {
-        swap_variable = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = swap_variable;
-      }
-    }
-  }
+for (iterator = 0; iterator < size - 1; ++iterator)
+{
+/*Loop through and compare elements*/
+for (i = 0; i < size - (iterator - 1); ++i)
+{
+/*change > to < for descending order*/
+if (array[i] > array[i + 1])
+{
+swap_variable = array[i];
+array[i] = array[i + 1];
+array[i + 1] = swap_variable;
+}
+}
+}
 }
